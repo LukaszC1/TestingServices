1e76d2690537b4f956258104e04e7b8637c7c9bc
Lukasz <lukaszrak01@gmail.com>
2025-06-08 12:10:25 +0200

    Move the CoreWCF project one directory up.

---
+++ TestingServices/CoreWCFService/Program.cs
@@ -0,0 +1,28 @@
+﻿using LocalRepository;
+
+var builder = WebApplication.CreateBuilder();
+
+builder.Services.AddServiceModelServices();
+builder.Services.AddServiceModelMetadata();
+builder.Services.AddSingleton<IRepository, LocalRepository.LocalRepository>();
+builder.Services.AddSingleton<SoapService>();
+
+builder.Services.AddSingleton<IServiceBehavior>(new ServiceBehaviorAttribute
+{
+    IncludeExceptionDetailInFaults = true
+});
+
+builder.Services.TryAddEnumerable(
+    ServiceDescriptor.Singleton<IServiceBehavior, UseRequestHeadersForMetadataAddressBehavior>());
+
+var app = builder.Build();
+
+app.UseServiceModel(serviceBuilder =>
+{
+    serviceBuilder.AddService<SoapService>()
+                  .AddServiceEndpoint<SoapService, ISoapService>(new BasicHttpBinding(), "/SoapService.svc");
+    var serviceMetadataBehavior = app.Services.GetRequiredService<ServiceMetadataBehavior>();
+    serviceMetadataBehavior.HttpGetEnabled = true;
+});
+    
+app.Run();
---
+++ TestingServices/CoreWCFService/SoapService.cs
@@ -0,0 +1,35 @@
+﻿using LocalRepository.DTO;
+using LocalRepository;
+
+namespace CoreWCFService;
+
+public class SoapService : ISoapService
+{
+    private readonly IRepository _repository;
+    public SoapService(IRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public Task<IEnumerable<Product?>> GetAllProductsAsync() => _repository.GetAllProductsAsync();
+
+    public Task<Customer> GetCustomerByIdAsync(string customerId) => _repository.GetCustomerByIdAsync(customerId);
+
+    public Task<IEnumerable<Customer>> GetAllCustomersAsync() => _repository.GetAllCustomersAsync();
+
+    public Task<IEnumerable<Order>> GetAllOrdersAsync() => _repository.GetAllOrdersAsync();
+
+    public Task<IEnumerable<OrderDetail>> GetOrderDetailsByOrderIdAsync(int orderId) => _repository.GetOrderDetailsByOrderIdAsync(orderId);
+
+    public Task<bool> AddCustomerAsync(Customer customer) => _repository.AddCustomerAsync(customer);
+
+    public Task<IEnumerable<Employee>> GetAllEmployeesAsync() => _repository.GetAllEmployeesAsync();
+
+    public Task<int> AddOrderAsync(Order order) => _repository.AddOrderAsync(order);
+
+    public Task<IEnumerable<OrderWithDetails>> GetOrdersWithDetailsAsync(int? orderId = null)
+        => _repository.GetOrdersWithDetailsAsync(orderId);
+
+    public Task<IEnumerable<CustomerWithOrders>> GetCustomerWithOrdersAsync(string? customerId = null)
+        => _repository.GetCustomerWithOrdersAsync(customerId);
+}
